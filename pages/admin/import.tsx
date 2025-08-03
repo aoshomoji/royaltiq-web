eabc1d2781346de8a8556806832991cf190fd1e7
Adebola Oshomoji <aoshomoji@gmail.com>
2025-08-03 20:34:36 +0100

    Fix import

--- pages/admin/import.tsx
+++ pages/admin/import.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { supabase } from '../utils/supabaseClient'
+import { supabase } from '../../utils/supabaseClient'
 
 export default function ImportPage() {
   const [artistId, setArtistId] = useState('')
